b7519347263c8040b6626dc7cdd3d92d82150803
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2021-09-01 20:45:39 +0400

    ...

--- geo-test/sw.js
+++ geo-test/sw.js
@@ -9,12 +9,12 @@ self.addEventListener('install', (e) => {
         });
 
 //self.addEventListener('fetch', (e) => {
-//        console.log(e.request.url);
 //        e.respondWith(
 //                caches.match(e.request).then((response) => response || fetch(e.request)),
 //                );
 //        });
 //
 
-self.addEventListener('fetch', function(e) {e.respondWith(caches.open('geo-test').then(function(cache) {return fetch(e.request).then(function(response) {cache.put(e.request, response.clone());return response;});}));});
+//self.addEventListener('fetch', function(e) {e.respondWith(caches.open('geo-test').then(function(cache) {return fetch(e.request).then(function(response) {cache.put(e.request, response.clone());return response;});}));});
 
+self.addEventListener('fetch', (e)=>{e.respondWith(caches.open('geo-test').then((cache)=>{fetch(e.request).then((response)=>{cache.put(e.request, response.clone());response;});}));});
