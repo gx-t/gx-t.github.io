651527b632db788e1a71544d03136439debe1807
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2021-09-02 23:36:34 +0400

    24 hour time

--- geo-test/geo-test.js
+++ geo-test/geo-test.js
@@ -71,7 +71,7 @@ function GeoTest() {
         ctx.textBaseline = 'bottom';
         ctx.font = s + 'pt Calibri';
         ctx.fillStyle = '#BBBBBBBB';
-        ctx.fillText('geo: ' + dt.toLocaleTimeString(), x, y); 
+        ctx.fillText('geo: ' + dt.toLocaleTimeString([], { hour12: false }), x, y); 
         ctx.restore();
     }
 
