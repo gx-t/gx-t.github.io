9eb1644467bd099c81535465eaad82d76f03a49a
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2021-09-04 23:49:30 +0400

    ...

--- geo-test/geo-test.js
+++ geo-test/geo-test.js
@@ -71,7 +71,7 @@ function GeoTest() {
         ctx.textBaseline = 'bottom';
         ctx.font = s + 'pt Calibri';
         ctx.fillStyle = '#BBBBBBBB';
-        ctx.fillText('S: ' + dt.toLocaleTimeString([], { hour12: false }), x, y); 
+        ctx.fillText('G: ' + dt.toLocaleTimeString([], { hour12: false }), x, y); 
         ctx.restore();
     }
 
