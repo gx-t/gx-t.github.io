bbf269147d32158b6d8d55d2e7dfae12df2e378a
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2021-09-04 23:21:41 +0400

    cache update

--- geo-test/sw.js
+++ geo-test/sw.js
@@ -1,6 +1,8 @@
+var cache_name = "geo-test-v0";
+
 self.addEventListener('install', (e) => {
         e.waitUntil(
-                caches.open('geo-test').then((cache) => cache.addAll([
+                caches.open(cache_name).then((cache) => cache.addAll([
                         'index.html',
                         'geo-test.js',
                         'img/Globe.png',
@@ -14,3 +16,18 @@ self.addEventListener('fetch', (e) => {
                 caches.match(e.request).then((response) => response || fetch(e.request)),
                 );
         });
+
+self.addEventListener('activate', (e) => {
+        console.log("*** activate ***");
+        e.waitUntil(caches.keys().then((key_list) => {
+                    Promise.all(key_list.map((key) => {
+                                if(key === cache_name) {
+                                console.log("not deleting: " + key);
+                                return;
+                                }
+                                console.log("deleting: " + key);
+                                caches.delete(key);
+                                }))
+                    }));
+        });
+
