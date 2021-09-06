572cb46ed71da83b32a2017729b6d73fc8cb3a11
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2021-09-07 00:05:02 +0400

    added data sharing, coordinates

--- geo-test/geo-test.js
+++ geo-test/geo-test.js
@@ -1,9 +1,44 @@
 function GeoTest() {
     var cs = document.createElement("canvas");
-    var gps = {coords: {altitude: 0,speed: 0}};
+    var gps = {coords: {altitude: 0,speed: 0,latitude: 0,longitude: 0}};
     var dt = new Date();
     var compass = {alpha: 0};
     var ctx = cs.getContext("2d");
+
+    main_menu = function() {
+        var div = document.createElement("div");
+        var timer = 0;
+
+        div.innerHTML = (navigator.share ? "<a href='javascript:share();'>Share ...</a><br>" : "") + "<a href='javascript:save();'>Save</a><hr>";
+        div.style.display = "none";
+        document.body.appendChild(div);
+        cs.onclick = function(e) {
+            if(div.style.display == "block") {
+                div.style.display = "none";
+                clearTimeout(timer);
+                return;
+            }
+            div.style = "position: absolute; left: 0px; top: 0px; display: block; background:black; font-size: " + Math.min(cs.width, cs.height) / 7 + "px";
+            console.log(e);
+            timer = setTimeout(() => {div.style.display = "none"}, 3000);
+        }
+
+        share = function() {
+            div.style.display = "none";
+            clearTimeout(timer);
+            navigator.share({
+                title: 'Geolocation data',
+                text: dt.toLocaleTimeString([], { hour12: false }) + "\n" +  Math.round(gps.coords.altitude) + "m\n" + Math.round(Math.round(gps.coords.speed * 3.6)) + "km/h\n",
+                url: "https://www.openstreetmap.org/?mlat=" + gps.coords.latitude + "&mlon=" + gps.coords.longitude + "#map=17/" + gps.coords.latitude + "/" + gps.coords.longitude,
+                });
+        }
+
+        save = function() {
+            div.style.display = "none";
+            clearTimeout(timer);
+        }
+
+    }
 
     keep_awake = async function() {
         if(!navigator.wakeLock)
@@ -65,13 +100,17 @@ function GeoTest() {
         update();
     }
 
-    draw_time = function(x, y, s) {
+    draw_coord_time = function(x, y, s) {
         ctx.save();
         ctx.textAlign = 'left';
         ctx.textBaseline = 'bottom';
         ctx.font = s + 'pt Calibri';
         ctx.fillStyle = '#BBBBBBBB';
         ctx.fillText('G: ' + dt.toLocaleTimeString([], { hour12: false }), x, y); 
+        y -= 3 * s;
+        ctx.fillText(gps.coords.latitude, x, y); 
+        y -= 1.5 * s;
+        ctx.fillText(gps.coords.longitude, x, y); 
         ctx.restore();
     }
 
@@ -79,7 +118,7 @@ function GeoTest() {
         ctx.font = cs.width * 0.125 + 'pt Calibri';
         ctx.fillText(Math.round(gps.coords.altitude), cs.width * 0.25, cs.width * 0.125); 
         ctx.fillText(Math.round(3.6 * gps.coords.speed), cs.width * 0.75, cs.width * 0.125); 
-        draw_time(cs.width / 25, cs.height, cs.width * 0.05);
+        draw_coord_time(cs.width / 25, cs.height, cs.width * 0.05);
         var r = cs.height * 0.15;
         compass_draw_proc(cs.width - r - cs.width / 25, cs.height - r, r, 0);
     }
@@ -88,7 +127,7 @@ function GeoTest() {
         ctx.font = cs.height * 0.125 + 'pt Calibri';
         ctx.fillText(Math.round(gps.coords.altitude), cs.height * 0.25, cs.height * 0.125); 
         ctx.fillText(Math.round(3.6 * gps.coords.speed), cs.height * 0.25, cs.height * 0.325); 
-        draw_time(cs.width / 25, cs.height, cs.height * 0.05);
+        draw_coord_time(cs.width / 25, cs.height, cs.height * 0.05);
         var r = cs.width * 0.15;
         compass_draw_proc(cs.width - r - cs.width / 25, cs.height - r, r, compass.gamma > 0 ? 90 : 270);
     }
@@ -127,18 +166,18 @@ function GeoTest() {
             DeviceOrientationEvent.requestPermission().then(function(resp) {
                     if("granted" != resp) {
                     alert("DeviceOrientationEvent.requestPermission - not granted");
-                    return;
                     }
                     clearInterval(it);
                     compass_draw_proc = draw_compass;
                     window.addEventListener("deviceorientation", on_orientation_change);
                     update();
                     });
-            cs.onclick = null;
+            main_menu();
         }
     }
     else {
         window.addEventListener("deviceorientationabsolute", on_orientation_change);
+        main_menu();
     }
     document.body.appendChild(cs);
     cs.update = update;
